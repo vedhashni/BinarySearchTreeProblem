b5f8713993a5f5f0181116349bc91f3278558eed
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-12 18:50:42 +0530

    [Vedhashni] Updated the file to do Insertion

---
+++ BinarySearchTree/BSTOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    /// <summary>
+    /// UC1 - Insertion Operation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class BSTOperations<T> where T : IComparable<T>
+    {
+        public T nodedata { get; set; }
+        public BSTOperations<T> leftnode { get; set; }
+
+        public BSTOperations<T> rightnode { get; set; }
+
+        public BSTOperations(T data)
+        {
+            this.nodedata = data;
+            this.leftnode = null;
+            this.rightnode = null;
+        }
+        int leftnodecount = 0, rightnodecount = 0;
+
+        public void Insert(T item)
+        {
+            T CurrNodeVal = this.nodedata;
+            if ((CurrNodeVal.CompareTo(item)) > 0)
+            {
+                if (this.leftnode == null)
+                {
+                    this.leftnode = new BSTOperations<T>(item);
+                }
+                else
+                {
+                    this.leftnode.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightnode == null)
+                {
+                    this.rightnode = new BSTOperations<T>(item);
+                }
+                else
+                {
+                    this.rightnode.Insert(item);
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftnode != null)
+            {
+                this.leftnodecount++;
+                this.leftnode.Display();
+            }
+            Console.WriteLine(this.nodedata.ToString());
+            if (this.rightnode != null)
+            {
+                this.rightnodecount++;
+                this.rightnode.Display();
+            }
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,6 +11,21 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Problem");
+            Console.WriteLine("1.Insertion.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            BSTOperations<int> bst = new BSTOperations<int>(56);
+            switch (option)
+            {
+                case 1:
+                    bst.Insert(30);
+                    bst.Insert(70);
+                    bst.Display();
+                    break;
+                default:
+                    
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
